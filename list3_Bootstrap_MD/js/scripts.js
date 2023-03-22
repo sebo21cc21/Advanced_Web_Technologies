7a5919e5d37d5ed8cdec198c55b2af99927ba491
wdomini4801 <91901855+wdomini4801@users.noreply.github.com>
2023-03-22 17:23:56 +0100

    Fix formatting

--- list3_Bootstrap_MD/js/scripts.js
+++ list3_Bootstrap_MD/js/scripts.js
@@ -3,13 +3,11 @@ AOS.init({
 })
 const ctx = document.getElementById("pieChart").getContext('2d');
 const myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
+    type: 'doughnut', data: {
         labels: ["Greece", "Bulgaria", "Spain", "Croatia", "France"],
         datasets: [{
             data: [450000, 400000, 1200000, 1100000, 340000],
-            backgroundColor: ["#0074D9", "#FF4136", "#2ECC40",
-                "#FF851B", "#7FDBFF"]
+            backgroundColor: ["#0074D9", "#FF4136", "#2ECC40", "#FF851B", "#7FDBFF"]
         }]
     },
 });
