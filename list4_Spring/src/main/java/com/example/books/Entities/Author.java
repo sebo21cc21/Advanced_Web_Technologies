7c1eef56c5e5fb9e421010622a894ef8f5f93e02
wdomini4801 <91901855+wdomini4801@users.noreply.github.com>
2023-03-28 22:49:20 +0200

    Add Author entity

---
+++ list4_Spring/src/main/java/com/example/books/Entities/Author.java
@@ -0,0 +1,12 @@
+package com.example.books.Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Author {
+    private int id;
+    private String name;
+    private String surname;
+}
