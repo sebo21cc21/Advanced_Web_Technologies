c490d99821a6bb7ef053a1d571514ae5bef65c9d
wdomini4801 <91901855+wdomini4801@users.noreply.github.com>
2023-03-29 00:30:25 +0200

    Create exceptions classes

---
+++ list4_Spring/src/main/java/com/example/books/Exceptions/InvalidObjectException.java
@@ -0,0 +1,12 @@
+package com.example.books.Exceptions;
+
+public class InvalidObjectException extends Exception {
+
+    public InvalidObjectException() {
+        super();
+    }
+
+    public InvalidObjectException(String message) {
+        super(message);
+    }
+}
