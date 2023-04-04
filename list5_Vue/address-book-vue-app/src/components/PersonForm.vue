9792c14d4e9462135b624d5bd3f705eff0ac14b6
Sebastian Bednarski <sebo21cc21@gmail.com>
2023-04-04 11:05:43 +0200

    Add list5

---
+++ list5_Vue/address-book-vue-app/src/components/PersonForm.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="person-form">
+      <form @submit.prevent="addPerson">
+        <label>Imię i nazwisko</label>
+        <input v-model="person.name" type="text" required />
+        <label>Email</label>
+        <input v-model="person.email" type="email" required />
+        <label>Telefon</label>
+        <input v-model="person.phone" type="tel" required />
+        <button type="submit">Dodaj kontakt</button>
+      </form>
+    </div>
+  </template>
+  <script>
+  export default {
+    name: "person-form",
+    data() {
+      return {
+        person: {
+          name: "",
+          email: "",
+          phone: "",
+        },
+      };
+    },
+    methods: {
+      addPerson() {
+        const newPerson = {
+          id: Date.now(),
+          name: this.person.name,
+          email: this.person.email,
+          phone: this.person.phone,
+        };
+        this.$emit("addPerson", newPerson);
+        this.person.name = "";
+        this.person.email = "";
+        this.person.phone = "";
+      },
+    },
+  };
+  </script>
+  <style scoped>
+  form {
+    margin-bottom: 2rem;
+  }
+  </style> 
---
+++ list5_Vue/address-book-vue-app/src/components/PersonsTable.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="persons-table">
+    <table>
+      <!-- ...thead... -->
+      <thead>
+         <tr>
+          <th>Imię i nazwisko</th>
+          <th>email</th>
+          <th>telefon</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="person in personsSource" :key="person.id">
+          <td>{{ person.name }}</td>
+          <td>{{ person.email }}</td>
+          <td>{{ person.phone }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  name: "persons-table",
+  props: {
+    personsSource: Array,
+  },
+};
+</script>
+<style scoped></style>
+
